6bd692d6c847cc6f6e3fc31fb70563e381772af4
Florian Vogt <florian@vorlif.org>
2023-12-07 23:25:22 +0100

    New Function: LanguageNameByCode

--- humanize/helper.go
+++ humanize/helper.go
@@ -16,6 +16,22 @@ func (h *Humanizer) LanguageName(lang string) string {
 	return h.loc.Get(lang)
 }
 
+// LanguageNameByCode returns the name of a language for a language code
+// in the national language of the Humanizer.
+//
+// If no language is known for the code, an empty string is returned.
+// If a language is known, but no translations are available, the English name is returned.
+//
+// For example, if a Humanizer was created for French, 'Anglais' is returned for the code 'en'
+// and the value 'Allemand' for the code 'de'.
+func (h *Humanizer) LanguageNameByCode(code string) string {
+	info, ok := LocaleInfos[code]
+	if !ok {
+		return ""
+	}
+	return h.loc.Get(info.Name)
+}
+
 // Language returns the currently used language.
 func (h *Humanizer) Language() language.Tag {
 	return h.loc.Language()
